943efa5bbceec22a7a01a8836866cacbb834b7d2
Saltal27 <omarmobarak53@gmail.com>
2023-06-13 00:01:53 +0300

    modified the services swiper

--- static/js/main.js
+++ static/js/main.js
@@ -214,6 +214,23 @@
   });
 
   /**
+   * Services details slider
+   */
+  new Swiper('.services-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
    * Testimonials slider
    */
   new Swiper('.testimonials-slider', {
